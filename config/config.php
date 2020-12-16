5644e92987ce5be4bc016abd4e59f7b88ecf9677
Hang <hangnguyetdung@gmail.com>
2020-12-16 08:50:31 +0700

    add images

--- config/config.php
+++ config/config.php
@@ -10,7 +10,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'Arrow@123');
 define('DB_HOST', 'localhost');
 
-define('BASE_PATH', 'http://localhost/framework');
+define('BASE_PATH', 'http://localhost:8012/framework');
 
 
 define('PAGINATE_LIMIT', '5');
