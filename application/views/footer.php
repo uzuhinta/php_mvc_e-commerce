bb4802b1a24f135ae3de8abca2d4ab5d3a65eb4e
Quan <quannar178@gmail.com>
2020-12-20 00:26:45 +0700

    Fix footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,5 @@
-<div>
-    <img style="width:100%; height: auto;" <?php echo $html->includeImg("footer", "png"); ?> alt="No" class="image-footer" />
-</div>
-</body>
-
+        <footer style="height: 1px;">
+            <img style="width:100%; height: auto;" <?php echo $html->includeImg("footer", "png"); ?> alt="No" class="image-footer" />
+        </footer>
+    </body>
 </html>
--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,55 +1,55 @@
-<main>
+    <main>
 
-    <div class="panel">
-        <img style="width: 100%; height: auto;" <?php echo $html->includeImg("nen",  "jpg"); ?> alt="No" class="image_main" />
-        <form>
-            <input id= "panel-input" type="imput" placeholder="Nhập món ăn ưa thích" >
-        </form>
-    </div>
-
-
-    <div class="salehot">
-        <img width="50" height="220" <?php echo $html->includeImg("salehot", "png"); ?> alt="No" />
-    </div>
-
-    <br/>
-    <div class="filter">
-        <div><img style="margin-left: 0px;" <?php echo $html->includeImg("sortBy", "png"); ?> alt="No" /></div>
-        <div><img <?php echo $html->includeImg("monMan", "png"); ?> alt="No" /></a></div>
-        <div><img <?php echo $html->includeImg("monNgot", "png"); ?> alt="No" /></a></div>
-        <div><img <?php echo $html->includeImg("comboXin", "png"); ?> alt="No" /></a></div>
-        <div><img <?php echo $html->includeImg("priceUp", "png"); ?> alt="No" /></a></div>
-        <div><img <?php echo $html->includeImg("priceDown", "png"); ?> alt="No" /></a></div>
-        <div><input type="text" name="" id="" placeholder="Tìm kiếm đồ ăn yêu thích" /></div>
-    </div>
-
-    <div class="list-post">
-        <?php foreach ($posts as $post): ?>
-        <div class="card">
-            <img <?php echo $html->includeImg("com-chay", "jpg"); ?> >
-            <div class="text">
-                <div class="price"><span><?php echo $post["Post"]["price"] ?> đ</span></div>
-                <div class="name"><span><?php echo $post["Post"]["title"] ?></span></div>
-            </div>
-            <?php if ($post["Post"]["sale"] != 0): ?>
-                <div class="sale"><span>Sale</span></div>
-            <?php endif ?>
+        <div class="panel">
+            <img style="width: 100%; height: auto;" <?php echo $html->includeImg("nen",  "jpg"); ?> alt="No" class="image_main" />
+            <form>
+                <input id= "panel-input" type="input" placeholder="Nhập món ăn ưa thích" >
+            </form>
         </div>
-        <?php endforeach; ?>
-    </div>
-
-    <div class="pagination">
-        <?php for ($i = 1; $i <= $totalPages; $i++):?>
-            <?php if ($i == $currentPageNumber):?>
-                <a class="active" ><?php echo $currentPageNumber?></a>
-            <?php else: ?>
-                <?php // echo $html->link($i,'posts/index/'.$i)?>
-                <a href= <?php echo $html->linkSrc("posts", "index", $i)?>  > <?php echo $i; ?> </a>
-            <?php endif?>
-        <?php endfor?>
-    </div>
 
 
+        <div class="salehot">
+            <img width="50" height="220" <?php echo $html->includeImg("salehot", "png"); ?> alt="No" />
+        </div>
 
-</main>
+        <br/>
+        <div class="filter">
+            <div><img style="margin-left: 0px;" <?php echo $html->includeImg("sortBy", "png"); ?> alt="No" /></div>
+            <div><img <?php echo $html->includeImg("monMan", "png"); ?> alt="No" /></a></div>
+            <div><img <?php echo $html->includeImg("monNgot", "png"); ?> alt="No" /></a></div>
+            <div><img <?php echo $html->includeImg("comboXin", "png"); ?> alt="No" /></a></div>
+            <div><img <?php echo $html->includeImg("priceUp", "png"); ?> alt="No" /></a></div>
+            <div><img <?php echo $html->includeImg("priceDown", "png"); ?> alt="No" /></a></div>
+            <div><input type="text" name="" id="" placeholder="Tìm kiếm đồ ăn yêu thích" /></div>
+        </div>
+
+        <div class="list-post">
+            <?php foreach ($posts as $post): ?>
+            <div class="card">
+                <img <?php echo $html->includeImg("com-chay", "jpg"); ?> >
+                <div class="text">
+                    <div class="price"><span><?php echo $post["Post"]["price"] ?> đ</span></div>
+                    <div class="name"><span><?php echo $post["Post"]["title"] ?></span></div>
+                </div>
+                <?php if ($post["Post"]["sale"] != 0): ?>
+                    <div class="sale"><span>Sale</span></div>
+                <?php endif ?>
+            </div>
+            <?php endforeach; ?>
+        </div>
+
+        <div class="pagination">
+            <?php for ($i = 1; $i <= $totalPages; $i++):?>
+                <?php if ($i == $currentPageNumber):?>
+                    <a class="active" ><?php echo $currentPageNumber?></a>
+                <?php else: ?>
+                    <?php // echo $html->link($i,'posts/index/'.$i)?>
+                    <a href= <?php echo $html->linkSrc("posts", "index", $i)?>  > <?php echo $i; ?> </a>
+                <?php endif?>
+            <?php endfor?>
+        </div>
+
+
+
+    </main>
 </div>
