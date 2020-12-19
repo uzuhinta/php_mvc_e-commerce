584e161490b2240e24dc3776782253c3bb1a58f2
Hang <hangnguyetdung@gmail.com>
2020-12-19 20:19:20 +0700

    add cart

--- application/views/users/cart.php
+++ application/views/users/cart.php
@@ -1,13 +1,70 @@
 <main>
     <div class="header-bottom">
-        <div class="login-form">
-            <h5>Địa chỉ nhận hàng</h5>
-            <input type="text" name="name" class="register-input1" placeholder="Họ và tên" />
-            <input type="text" name="phone" class="register-input1" style="margin-left:2.9%;" placeholder="SĐT" />
+        <div class="login-form" style="max-width: 55rem;">
+            <p class="title" style="text-align: center;">Thông tin</p>
+            <br>
+            <input type="text" name="name" class="register-input1" placeholder="Mèo béo" />
+            <input type="text" name="phone" class="register-input1" style="margin-left:2.9%;" placeholder="0353253464" />
 
-            <input type="text" name="address" class="register-input2" placeholder="Địa chỉ" />
-            <input type="text" name="email" class="register-input2" placeholder="Email" />
+            <input type="text" name="address" class="register-input2" placeholder="Ngõ tự do" />
+            <input type="text" name="note" class="register-input2" placeholder="Ghi chú: cho thêm nhiều thịt nhé" />
         </div>
+        <p class="title" style="text-align: center; font-size: 32px;">Giỏ hàng có gì?</p>
+        <table>
+            <thead>
+                <th>
+                    STT
+                </th>
+                <th>
+                    Tên sản phẩm
+                </th>
+                <th>
+                    Số lượng
+                </th>
+                <th>
+                    Giá tiền
+                </th>
+                <th>
+                    Thành tiền
+                </th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Cơm cháy</td>
+                    <td>
+                        <button>-</button>
+                        1
+                        <button>+</button>
+                    </td>
+                    <td>20.000 đ</td>
+                    <td>20.000 đ</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Cơm cháy</td>
+                    <td>
+                        <button>-</button>
+                        1
+                        <button>+</button>
+                    </td>
+                    <td>20.000 đ</td>
+                    <td>20.000 đ</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Cơm cháy</td>
+                    <td>
+                        <button>-</button>
+                        1
+                        <button>+</button>
+                    </td>
+                    <td>20.000 đ</td>
+                    <td>20.000 đ</td>
+                </tr>
+            </tbody>
+        </table>
+        <p class="title" style="text-align: left; font-size: 32px; margin-top: 6px;">Thành tiền: 60.000Đ</p>
     </div>
 </main>
 </div>
