84cced60a1a3ec4ca0b04ece7b3683645721efd5
Hang <hangnguyetdung@gmail.com>
2020-12-21 00:20:32 +0700

    add contact

---
+++ application/views/users/contact.php
@@ -0,0 +1,47 @@
+<div id="main">
+    <img <?php echo $html->includeImg("nen1", "jpg"); ?> alt="No" class="image_main" />
+    <card class="card_contact">
+        <form class="login-form" style="padding-bottom: 0px" autocomplete="off">
+            <p class="title" style="text-align: center;">Liên hệ với chúng tôi ^ - ^</p>
+            <table>
+                <thead>
+                    <th class="text_center">
+                        PING PONG
+                    </th>
+                    <th class="text_center">
+                        Liên hệ
+                    </th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Với phương châm sự hài lòng của bạn là sự thành công của chúng tôi, Ping Pong luôn mong muốn nhận được các góp
+                            ý tích cực từ bạn để có thể ngày một hoàn thiện hơn, trở thành địa chỉ đáng tin cậy
+                            để bạn trao trọn tin yêu. Đừng ngần ngại mà hãy liên hệ với cúng tôi ngay nhé!
+                        </td>
+                        <td>Bạn có thể liên hệ qua các phương thức sau:
+                            <br>
+                            Địa chỉ:
+                            <a class="color_text" href="https://www.google.com/maps/place/100+Tr%E1%BA%A7n+%C4%90%E1%BA%A1i+Ngh%C4%A9a,+%C4%90%E1%BB%93ng+T%C3%A2m,+Hai+B%C3%A0+Tr%C6%B0ng,+H%C3%A0+N%E1%BB%99i,+Vi%E1%BB%87t+Nam/@20.9977709,105.836974,15z/data=!3m1!4b1!4m5!3m4!1s0x3135ac71e4fe4997:0xb66ccd412e437cbc!8m2!3d20.9977711!4d105.8457288?hl=vi-VN" target="_blank">
+                                100 Trần Đại Nghĩa, Hai Bà Trưng, Hà Nội
+                            </a>
+                            <br>
+                            Phone: 0912345678
+                            <br>
+                            Email:
+                            <a class="color_text" href="mailto:hangnguyetdung@gmail.com?subject=Feedback&body=Message">
+                                pingpong.happyfood@gmail.com
+                            </a>
+                            <br>
+                            <a class="color_text" href="https://www.facebook.com/doanvathanoi1" target="_blank">Facebook</a>
+                            <br>
+                            <a class="color_text" href="https://www.instagram.com/douyin_tik_tok/" target="_blank">Instagram</a>
+                            <br>
+                            <a class="color_text" href="https://shopee.vn/monquatuthiennhien?categoryId=9824&itemId=2424472746" target="_blank">Shopee</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    </card>
+</div>
+</div>
