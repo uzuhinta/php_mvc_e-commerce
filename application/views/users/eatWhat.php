67bb132ff1063f932ca843ebd352556fb202e100
Hang <hangnguyetdung@gmail.com>
2020-12-20 10:42:52 +0700

    edit cart and add eat what

---
+++ application/views/users/eatWhat.php
@@ -0,0 +1,24 @@
+	<main>
+	    <div class="detail_main">
+	        <div class="detail">
+	            <div class="card_detail">
+	                <div class="images">
+	                    <a href="#">
+	                        <img <?php echo $html->includeImg("nem-nuong", "png"); ?> alt="No" />
+	                    </a>
+	                </div>
+	                <div class="login-form">
+	                    <h2>Nem nướng Nha Trang</h2>
+	                    <p>Trong tiềm thức của các bạn sinh viên Bách - Kinh - Xây, nem nướng Hà Anh là quán ăn vặt bình dân, đồ ăn rẻ lại chất lượng.
+	                        Quán phục vụ nem nướng, bánh bột lọc và một vài loại nước giải khát.
+	                        Nem nướng ở đây rất thơm, làm chuẩn chỉ theo công thức Nha Trang, nướng thủ công trên bếp than.
+	                        Những nguyên liệu như: rau xà lách, dưa chuột có xuất xứ từ Đà Lạt. Và một yếu tố quyết định độ ngon của nem nướng là nước chấm.
+	                        Nước chấm Hà Anh có độ sánh đặc, thơm mùi hành phi, hơi le te cay nơi đầu lưỡi.</p>
+	                </div>
+
+	            </div>
+	        </div>
+
+	    </div>
+	</main>
+	</div>
