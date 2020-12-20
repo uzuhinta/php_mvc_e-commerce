8af3680747599583479503fa4c73d63981e4b76e
Quan <quannar178@gmail.com>
2020-12-20 13:04:01 +0700

    add css body

--- application/views/header.php
+++ application/views/header.php
@@ -7,6 +7,7 @@
 	<?php echo $html->includeCss("header"); ?>
 	<?php echo $html->includeCss("style_login"); ?>
 	<?php echo $html->includeCss("style_main"); ?>
+    <?php echo $html->includeCss("body"); ?>
 	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
