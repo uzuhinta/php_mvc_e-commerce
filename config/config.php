0c0de115b254f718d84015892be78b470fc956c5
Nguyen Ba Quan <31834222+quannar178@users.noreply.github.com>
2020-12-23 02:37:08 +0700

    Match jaw

--- config/config.php
+++ config/config.php
@@ -5,12 +5,12 @@
 define('DEVELOPMENT_ENVIRONMENT', true);
 
 
-define('DB_NAME', 'framework');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '123456');
-define('DB_HOST', 'localhost');
+define('DB_NAME', 'jsexdv14qmbhmogx');
+define('DB_USER', 'h3f5a98f0wojftl6');
+define('DB_PASSWORD', 'mwhxkmbuep457hlq');
+define('DB_HOST', '	dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 
-define('BASE_PATH', 'http://localhost/framework');
+define('BASE_PATH', 'https://pingpong-dhqt.herokuapp.com');
 
 
 define('PAGINATE_LIMIT', '5');
