2c19a4674ce9c80703c8d57aeaec6cf02d97b2da
Nguyen Ba Quan <31834222+quannar178@users.noreply.github.com>
2020-12-23 01:43:11 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@
 define('DEVELOPMENT_ENVIRONMENT', false);
 
 
-define('DB_NAME', 'jsexdv14qmbhmogx	');
+define('DB_NAME', 'jsexdv14qmbhmogx');
 define('DB_USER', 'h3f5a98f0wojftl6');
 define('DB_PASSWORD', 'mwhxkmbuep457hlq');
 define('DB_HOST', 'dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
