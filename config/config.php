7a64f790990241cbebb7408de2d0237bae775cd7
Nguyen Ba Quan <31834222+quannar178@users.noreply.github.com>
2020-12-23 01:43:52 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -10,7 +10,7 @@ define('DB_USER', 'h3f5a98f0wojftl6');
 define('DB_PASSWORD', 'mwhxkmbuep457hlq');
 define('DB_HOST', 'dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 
-define('BASE_PATH', 'https://pingpong-dhqt.herokuapp.com/');
+define('BASE_PATH', 'https://pingpong-dhqt.herokuapp.com');
 
 
 define('PAGINATE_LIMIT', '5');
