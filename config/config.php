1546a71a1329c56f6c5769a7cbe7d34727521de4
Nguyen Ba Quan <31834222+quannar178@users.noreply.github.com>
2020-12-23 00:43:19 +0700

    heroku deploy

--- config/config.php
+++ config/config.php
@@ -5,10 +5,10 @@
 define('DEVELOPMENT_ENVIRONMENT', false);
 
 
-define('DB_NAME', 'framework');
+define('DB_NAME', 'jsexdv14qmbhmogx');
 define('DB_USER', 'h3f5a98f0wojftl6');
 define('DB_PASSWORD', 'mwhxkmbuep457hlq');
-define('DB_HOST', 'jsexdv14qmbhmogx');
+define('DB_HOST', 'dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 
 define('BASE_PATH', 'https://pingpong-dhqt.herokuapp.com/');
 
